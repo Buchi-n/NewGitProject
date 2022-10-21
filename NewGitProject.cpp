d9978241e4922b9e675b70684c78b273e50030fa
大輝 池淵 <k022g1051@m.neec.ac.jp>
2022-10-21 09:39:07 +0900

    New Git Projectに変更

--- NewGitProject.cpp
+++ NewGitProject.cpp
@@ -7,7 +7,7 @@ int main()
 {
     std::cout << "Hello VisualStudio Git!\n";
    
-    std::cout << "New Branch Changed\n";
+    std::cout << "New Git Project\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
