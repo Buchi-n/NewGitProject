fbc28438baca001d41cb9470e7080cc81005f37e
大輝 池淵 <k022g1051@m.neec.ac.jp>
2022-11-04 09:15:16 +0900

    third branchに修正

--- NewGitProject.cpp
+++ NewGitProject.cpp
@@ -8,6 +8,8 @@ int main()
     std::cout << "Hello VisualStudio Git!\n";
    
     std::cout << "New Git Project\n";
+
+    std::cout << "third branch\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
